6085e3a53eff337993a9531416f873abff565dc8
Andrew Lochow <Andrew Lochow@192.168.0.9>
2017-05-28 14:37:07 -0600

    SQLLite JDBC Checkin

---
+++ SQLiteJDBCDriverConnection/src/net/sqlitejdbcdriverconnection/DBCreation.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.sqlitejdbcdriverconnection;
+
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+ 
+/**
+ *
+ * @author Andrew Lochow
+ */
+
+
+public class DBCreation {
+ 
+    /**
+     * Connect to a sample database
+     *
+     * @param fileName the database file name
+     */
+    public static void createNewDatabase(String fileName) {
+ 
+        String url = "jdbc:sqlite:C:/sqlite/db/" + fileName;
+ 
+        try (Connection conn = DriverManager.getConnection(url)) {
+            if (conn != null) {
+                DatabaseMetaData meta = conn.getMetaData();
+                System.out.println("The driver name is " + meta.getDriverName());
+                System.out.println("A new database has been created.");
+            }
+ 
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+ 
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        createNewDatabase("test.db");
+    }
+}
---
+++ SQLiteJDBCDriverConnection/src/net/sqlitejdbcdriverconnection/Query_Persons.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.sqlitejdbcdriverconnection;
+
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+/**
+ *
+ * @author Andrew Lochow
+ */
+public class Query_Persons {
+ 
+    /**
+     * Connect to the test.db database
+     * @return the Connection object
+     */
+    private Connection connect() {
+        // SQLite connection string
+        String url = "jdbc:sqlite:C://sqlite/db/test.db";
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection(url);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return conn;
+    }
+ 
+    
+    /**
+     * select all rows in the warehouses table
+     */
+    public void selectAll(){
+        String sql = "SELECT id, name, notes FROM person";
+        
+        try (Connection conn = this.connect();
+             Statement stmt  = conn.createStatement();
+             ResultSet rs    = stmt.executeQuery(sql)){
+            
+            // loop through the result set
+            while (rs.next()) {
+                System.out.println(rs.getInt("id") +  "\t" + 
+                                   rs.getString("name") + "\t" +
+                                   rs.getString("notes"));
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+    
+   
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Query_Persons app = new Query_Persons();
+        app.selectAll();
+    }
+ 
+}
