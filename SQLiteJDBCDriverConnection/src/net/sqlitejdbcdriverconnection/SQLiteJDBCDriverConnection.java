8718571c12543fa180cfeb0d4275317574aeb923
Andrew Lochow <Andrew Lochow@192.168.0.9>
2017-05-28 13:47:44 -0600

    SQLLite JDBC Checkin

--- SQLiteJDBCDriverConnection/src/net/sqlitejdbcdriverconnection/SQLiteJDBCDriverConnection.java
+++ SQLiteJDBCDriverConnection/src/net/sqlitejdbcdriverconnection/SQLiteJDBCDriverConnection.java
@@ -4,17 +4,45 @@
  * and open the template in the editor.
  */
 package net.sqlitejdbcdriverconnection;
-
+ 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 /**
  *
  * @author Andrew Lochow
  */
 public class SQLiteJDBCDriverConnection {
-
+     /**
+     * Connect to a sample database
+     */
+    public static void connect() {
+        Connection conn = null;
+        try {
+            // db parameters
+            String url = "jdbc:sqlite:C:/sqlite/db/test.db";
+            // create a connection to the database
+            conn = DriverManager.getConnection(url);
+            
+            System.out.println("Connection to SQLite has been established.");
+            
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            try {
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException ex) {
+                System.out.println(ex.getMessage());
+            }
+        }
+    }
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        connect();
         // TODO code application logic here
     }
     
