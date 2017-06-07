bc7f1ba9e14e76a3e67ce4fe251413e5b744ef20
Andrew Lochow <andrew.lochow@issinc.com>
2017-06-07 17:35:36 -0600

    Property Database commit code

--- SWE482/src/swe482_Main/MProperty.java
+++ SWE482/src/swe482_Main/MProperty.java
@@ -3,6 +3,11 @@ package swe482_Main;
  *
  * @author by Michael Barth
  */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class MProperty{
 
     MProperty() {
@@ -13,7 +18,7 @@ public class MProperty{
 
     // Variable Declaration Area:  Do not change.
 
-    private int dbRecordID;
+    private String dbRecordID;
     private int parcelID;
     private String taxMapID;
     private String county;
@@ -26,10 +31,23 @@ public class MProperty{
     private String description;
     private final String[] bounders = new String[4];
     // TO DO: Create List<E> to hold RTI MInterest recods
+    
+    private Connection connect() {
+        // SQLite connection string
+        String url = "jdbc:sqlite:./db/landman.db";
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection(url);
+            System.out.println("Database Connection Established");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return conn;
+    }
     // END Variable Declaration Area
  
     void setValues(
-            int dbRecordID,
+            String dbRecordID,
             int parcelID,
             String taxMapID,
             String county,
@@ -62,9 +80,40 @@ public class MProperty{
         this.bounders[1] = b_east;
         this.bounders[2] = b_south;
         this.bounders[3] = b_west;
+        
+        
         // TO DO: Create List<E> to hold RTI MInterest recods
+        
+         
 
     }
     // setValues()
+    
+    //Commit Section Added 6/7/2017 by Andrew Lochow
+    
+    public void committoDB(){
+        
+        String sql = "INSERT INTO Property (LandRecordID,ID_Parcel,ID_Tax, Description, Acres) VALUES(?,?,?,?,?)";
+        Connection conn = this.connect();
+        PreparedStatement pstmt = null;
+        try{
+            pstmt = conn.prepareStatement(sql);
+            pstmt.setString(1, this.dbRecordID);
+            pstmt.setInt(2, this.parcelID);
+            pstmt.setString(3, this.taxMapID);
+            pstmt.setString(4, this.description);
+            pstmt.setInt(5, this.acreage);
+            conn.commit();
+            pstmt.close();
+            conn.setAutoCommit(true);
+        }
 
+        catch(SQLException e){
+            System.out.println(e.getMessage());
+        }       
+        
+    }
+    
 }
+
+   
