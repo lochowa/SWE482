f3b058b8ece79baa67155f5abac001086f73be3f
Andrew Lochow <lochow.andrew@gmail.com>
2017-06-06 22:00:50 -0600

    It connects!

--- SWE482/src/Create_Database/CreateTables.java
+++ SWE482/src/Create_Database/CreateTables.java
@@ -9,6 +9,38 @@ package Create_Database;
  *
  * @author Andrew Lochow
  */
-public class CreateTables {
-    
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+ 
+public class QueryPersons {
+ 
+    /**
+     * Create a new table in the test database
+     *
+     */
+    public static void createNewTable() {
+        // SQLite connection string
+        String url = "jdbc:sqlite:C://sqlite/db/test.db";
+        
+        // SQL statement selecting data in table
+        String sql = "Select * From Persons";
+        
+        try (Connection conn = DriverManager.getConnection(url);
+                Statement stmt = conn.createStatement()) {
+            // select statement
+            stmt.execute(sql);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+ 
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        createNewTable();
+    }
+ 
 }
