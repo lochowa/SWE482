6e6384e1453fa6c79ad6152f1a147a3e9de46b8e
Andrew Lochow <andrew.lochow@issinc.com>
2017-06-13 13:19:24 -0600

    Property Database commit code

--- SWE482/src/swe482_Main/UC001Model.java
+++ SWE482/src/swe482_Main/UC001Model.java
@@ -29,6 +29,11 @@ public class UC001Model{
     private int section;
     private String Meridian;
     private String description;
+    private String PM;
+    private String b_north;
+    private String b_east;
+    private String b_south;
+    private String b_west;
     private final String[] bounders = new String[4];
     // TO DO: Create List<E> to hold RTI MInterest recods
     
@@ -39,19 +44,9 @@ public class UC001Model{
         Connection conn = null;
         try {
             conn = DriverManager.getConnection(url);
-            System.out.println("Database Connection Established");
+            return conn;
         } catch (SQLException e) {
             System.out.println(e.getMessage());
-        }
-        finally {
-            try {
-                if (conn != null) {
-                    System.out.println("Could not connect to Database");
-                    conn.close();
-                }
-            } catch (SQLException ex) {
-                System.out.println(ex.getMessage());
-            }
         }
         return conn;
     }
@@ -75,6 +70,21 @@ public class UC001Model{
             String b_west
     // TO DO: Integrate List<E>
     ) {
+        this.dbRecordID = dbRecordID;
+        this.parcelID = parcelID;
+        this.taxMapID = taxMapID;
+        this.county = county;
+        this.state = state;
+        this.acreage = acreage;
+        this.township = township;
+        this.range = range;
+        this.section = section;
+        this.PM = PM;
+        this.description = description;
+        this.b_north = b_north;
+        this.b_east = b_east;
+        this.b_south = b_south;
+        this.b_west = b_west;
         
         // TO DO: Create List<E> to hold RTI MInterest recods
         
@@ -85,21 +95,24 @@ public class UC001Model{
     
     //Commit Section Added 6/7/2017 by Andrew Lochow
     
-    public void committoDB(){
-        
-        String sql = "INSERT INTO Property (LandRecordID,ID_Parcel,ID_Tax, Description, Acres) VALUES(?,?,?,?,?)";
-        Connection con = this.connect();
+    public void committoDB() throws SQLException {
+        Connection con = null;
         PreparedStatement pstmt = null;
+        String sql = "INSERT INTO Property"
+                + "(LandRecordID,ID_Parcel,ID_Tax, Description, Acres)"
+                + "VALUES(?,?,?,?,?)";
+
         try{
-            con.setAutoCommit(false);
+            con = this.connect();
             pstmt = con.prepareStatement(sql);
-            pstmt.setString(1, this.dbRecordID);
-            pstmt.setInt(2, this.parcelID);
+            pstmt.setString(1,this.dbRecordID);
+            pstmt.setInt(2,this.parcelID);
             pstmt.setString(3, this.taxMapID);
-            pstmt.setString(4, this.description);
-            pstmt.setInt(5, this.acreage);
-            con.commit();
-            pstmt.close();
+            pstmt.setString(4,this.description);
+            pstmt.setInt(5,this.acreage);        
+            pstmt.executeUpdate();
+            System.out.println("Record is inserted into Property table!");
+           
         }
 
         catch(SQLException e){
